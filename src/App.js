00f212eba05b017eba0c5e5a903ed05fc3437ef7
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-10-04 09:30:28 +0100

    ADD component routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -8,7 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
