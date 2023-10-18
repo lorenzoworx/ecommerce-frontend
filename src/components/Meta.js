8a3bee43f24da1571a7dd17afb9cdc5a0ce6400d
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-10-18 10:48:43 +0100

    ADD meta component

---
+++ src/components/Meta.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = (props) => {
+  const {title} = props;
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>{ title }</title>
+    </Helmet>
+  )
+};
+
+export default Meta;
