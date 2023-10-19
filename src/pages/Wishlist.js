e1cd71716bccc449fefa5246fed624f4ba256bde
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-10-19 23:06:01 +0100

    ADD wishlist page

---
+++ src/pages/Wishlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+
+const Wishlist = () => {
+  return(
+    <>
+      <Meta title="Wishlist" />
+      <BreadCrumb title="Wishlist" />
+      <div>Hello Text</div>
+    </>
+  )
+};
+
+export default Wishlist;
