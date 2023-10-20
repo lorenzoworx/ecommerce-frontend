4c578e986c8023b696c363296f29e12a265e46f9
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-10-20 21:30:54 +0100

    ADD routes for policy pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import SingleBlog from './pages/SingleBlog';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import RefundPolicy from './pages/RefundPolicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import TermsAndConditions from './pages/TermsAndConditions';
 
 function App() {
   return (
@@ -33,6 +37,11 @@ function App() {
             <Route path="sign-up" element={<SignUp />} />
             <Route path="forgot-password" element={<ForgotPassword />} />
             <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="refund-policy" element={<RefundPolicy />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="shipping-policy" element={<ShippingPolicy />} />
+            <Route path="terms-conditions" element={<TermsAndConditions />} />
           </Route>
         </Routes>
       </BrowserRouter>
