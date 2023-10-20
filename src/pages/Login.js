c1dbbee3b0553b568c7ced48c63a7782c025dda5
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-10-20 19:07:42 +0100

    ADD user login page

---
+++ src/pages/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/Meta';
+
+const Login = () => (
+  <>
+    <Meta title="Login" />
+    <BreadCrumb title="Login" />
+    <div className="login-wrapper home-wrapper-2 py-5">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="login-card">
+              <h3 className="text-center mb-3">Login</h3>
+              <form action="" className="d-flex flex-column gap-15">
+                <div>
+                  <input type="email" name="email" placeholder="Email" className="form-control" />
+                </div>
+                <div className="mt-1">
+                  <input type="password" name="password" placeholder="Password" className="form-control" />
+                </div>
+                <div>
+                  <Link to="/forgot-password">Forgot Password?</Link>
+                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                    <button type="submit" className="button border-0">Login</button>
+                    <Link to="/sign-up" className="button signup">Sign Up</Link>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </>
+);
+
+export default Login;
